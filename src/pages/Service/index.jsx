04c4c10937bd789ecdb91f282f2ceb080beee728
Daryl Reyes <darylhreyes@gmail.com>
2024-05-15 15:48:05 +0800

    services

--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -8,25 +8,25 @@ function Service() {
       title: "Customs Brokerage Services",
       description:
         "We handle all the necessary paperwork and logistics involved in customs clearance for importing and exporting goods, ensuring a smooth and efficient process for our clients.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/doc-prod.png")]`
     },
     {
       title: "International Freight Forwarding (Door to Port)",
       description:
         "Our service covers the transportation of goods from the doorstep of your business to the designated port of arrival, simplifying the complexities of global shipping and ensuring timely delivery.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/ship.png")]`
     },
     {
       title: "Domestic Freight Forwarding (Door to Door)",
       description:
         "We offer comprehensive freight forwarding solutions for domestic shipments, managing the entire transportation process from pickup at your location to delivery directly to the desired destination, providing convenience and peace of mind.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/truck-cargo.png")]`
     },
     {
       title: "Hauling / Trucking for Domestic and International",
       description:
         "Our hauling and trucking services cater to both domestic and international shipments, offering reliable transportation solutions to move your goods safely and efficiently, whether it's across town or across borders.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/truck-cargo1.png")]`
     }
   ];
 
@@ -35,37 +35,37 @@ function Service() {
       title: "Tariff and Customs Consultancy",
       description:
         "Our team provides clear advice on tariffs and customs regulations, helping you navigate import/export processes with ease.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/consult.png")]`
     },
     {
       title: "IMPORT/EXPORT Documentation",
       description:
         "We take care of all the paperwork, ensuring your shipments move smoothly from point A to point B.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/ie-service.png")]`
     },
     {
       title: "Transhipments",
       description:
         "Seamlessly transferring goods between different modes of transport to keep your supply chain flowing.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/transhipment.png")]`
     },
     {
       title: "Certificate of Payment Processing (For Motor Vehicle)",
       description:
         "Let us handle the paperwork needed to process payment certificates for your vehicles hassle-free.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/payment.png")]`
     },
     {
       title: "LTO Processing (For Motor Vehicle)",
       description:
         "We manage the paperwork for securing necessary approvals from the Land Transportation Office, making vehicle processing a breeze.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/process.png")]`
     },
     {
       title: "Importer's Accreditation Processing",
       description:
         "We assist in the accreditation process, making sure your import operations meet all regulatory requirements without any hassle.",
-      icon: ` bg-[url("/assets/img/values.png")]`
+      icon: ` bg-[url("/assets/img/iap-service.png")]`
     }
   ];
   return (
@@ -86,36 +86,16 @@ function Service() {
           every step of the journey.
         </p>
       </div>
-      {/* <div className="grid grid-row-2 md:grid-cols-2 px-5 gap-4 my-16 ">
-        <div className="flex flex-col gap-8  justify-center  items-center lg:px-5 xl:px-20">
-          <h1 className="font-extrabold text-center text-[70px]">
-            PRODUCT <br /> & <br /> SERVICES
-          </h1>
-        </div>
-        <div className="flex flex-col gap-8 lg:px-20  justify-center items-center  ">
-          <div>
-            <p className="text-[20px]">
-              At{" "}
-              <span className="text-[30px] font-bold">
-                Erth Liner Cargo Express Co.
-              </span>
-              , we firmly believe that QUALITY SERVICE is not just a goal but
-              the very foundation upon which all our efforts are built. We
-              understand that the satisfaction of both our clients and
-              stakeholders is paramount, and it's this guiding principle that
-              drives every aspect of our operation. From the moment you engage
-              with us to the final delivery of your goods, our commitment to
-              excellence shines through at every step of the journey.
-            </p>
-          </div>
-        </div>
-      </div> */}
+
       <div className="px-5 gap-4 my-16 flex flex-col items-center ">
         <div className="text-[60px] font-extrabold">Products</div>
-        <div className="grid  md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 gap-4 lg:grid-rows-1">
-          {products.map((product) => {
+        <div className="flex flex-wrap justify-center items-stretch gap-4">
+          {products.map((product, index) => {
             return (
-              <div className="flex flex-col gap-4 p-5 shadow-xl">
+              <div
+                className="flex flex-col gap-4 p-5 shadow-xl w-[350px] lg:w-[380px]"
+                key={index}
+              >
                 <div className="flex justify-center">
                   <div
                     className={` ${product?.icon} h-[60px] w-[60px] bg-cover bg-no-repeat bg-center `}
@@ -131,12 +111,36 @@ function Service() {
         </div>
       </div>
 
-      <div className=" px-5 gap-4 my-16 flex flex-col items-center ">
+      <div className="px-5 gap-4 my-16 flex flex-col items-center ">
+        <div className="text-[60px] font-extrabold">Services</div>
+        <div className="flex flex-wrap justify-center items-stretch gap-4">
+          {services.map((service, index) => {
+            return (
+              <div
+                className="flex flex-col gap-4 p-5 shadow-xl w-[350px] lg:w-[500px]"
+                key={index}
+              >
+                <div className="flex justify-center">
+                  <div
+                    className={` ${service?.icon} h-[60px] w-[60px] bg-cover bg-no-repeat bg-center `}
+                  />
+                </div>
+                <h1 className="text-center text-[30px] font-extrabold">
+                  {service?.title}
+                </h1>
+                <p>{service?.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <div className=" px-5 gap-4 my-16 flex flex-col items-center lg:px-20">
         <div className="text-[60px] font-extrabold">Services</div>
         <div className="grid  md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 gap-4 lg:grid-rows-2">
-          {services.map((services) => {
+          {services.map((services, index) => {
             return (
-              <div className="flex flex-col gap-4 p-5 shadow-xl">
+              <div className="flex flex-col gap-4 p-5 shadow-xl" key={index}>
                 <div className="flex justify-center">
                   <div
                     className={` ${services?.icon} h-[60px] w-[60px] bg-cover bg-no-repeat bg-center `}
@@ -150,7 +154,7 @@ function Service() {
             );
           })}
         </div>
-      </div>
+      </div> */}
     </MainLayout>
   );
 }
